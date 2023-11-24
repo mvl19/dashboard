3bd4ed94731ab955bb48857d61cd603deb5069ed
mvl19 <56024956+mvl19@users.noreply.github.com>
2023-11-24 15:41:31 +0700

    refactor: add props type checking

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,25 @@
-import { RiseOutlined, MenuFoldOutlined, DownOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
-import { Avatar, Dropdown } from 'antd'
+import { RiseOutlined, MenuFoldOutlined, DownOutlined, MenuUnfoldOutlined, SearchOutlined } from '@ant-design/icons'
+import { Avatar, Dropdown, MenuProps, Space, Button, Input } from 'antd'
 import { useEffect, useState } from 'react'
-import { MenuProps, Space, Button } from 'antd'
+
 interface Dropdown {
   label: string,
-  key: number
+  key: string,
+  type: string,
 }
-export default function Navbar(props: any) {
-    const WHITE_COLOR = '#FCFCFC'
+
+interface Collapsible {
+  func: (collapse: boolean) => void
+}
+
+enum Colors {
+  WHITE_COLOR = '#fcfcfc',
+  TEXT_COLOR = '#f8fafc',
+  BLUE_COLOR = '#0588f0',
+}
+
+export default function Navbar(props: Collapsible) {
+  
     const items: MenuProps['items'] = [
         {
           label: 'Placeholder 1',
@@ -26,34 +38,38 @@ export default function Navbar(props: any) {
         },
       ];
       const [item, setItem] = useState<Dropdown[]>(items[3])
-      const onClick: MenuProps['onClick'] = ({ key }) => {
+      const onClick: MenuProps['onClick'] = ({ key }): void => {
+        console.log(Colors)
         setItem(items[key])
       };
       const [collapse, setCollapse] = useState(false)
       useEffect(() => {
         props.func(collapse)
-      }, [collapse])
+      }, [collapse, props])
 
     return (
         <div className="flex justify-center align-center sticky top-0 z-20 ">
-            <div className="w-[100vw] h-auto max-h-[72px] bg-[#0588F0] flex justify-between gap-[16px] items-center p-[24px] sticky top-0 z-20 shadow-[0_1px_10px_rgba(0,0,0,0.5)]">
+            <div className={'w-[100vw] h-auto max-h-[72px] bg-[#0588f0] flex justify-between gap-[16px] items-center p-[24px] sticky top-0 z-20 shadow-[0_1px_10px_rgba(0,0,0,0.5)]'}>
                 <div className="flex items-center gap-[6px]">
                   <Button
                     type='text'
                     icon={
                       collapse? 
-                      <MenuUnfoldOutlined style={{color: WHITE_COLOR}} /> 
+                      <MenuUnfoldOutlined style={{color: Colors.WHITE_COLOR}} /> 
                       : 
-                      <MenuFoldOutlined style={{color: WHITE_COLOR}} />
+                      <MenuFoldOutlined style={{color: Colors.WHITE_COLOR}} />
                     }
                     onClick={() => setCollapse(!collapse)}
                   />
-                    <RiseOutlined style={{color: WHITE_COLOR}} />
-                    <span className="text-[#f8fafc]">Dashboard Admin</span>
+                    <RiseOutlined style={{color: Colors.WHITE_COLOR}} />
+                    <span className={"text-[#f8fafc]"}>Dashboard Admin</span>
                 </div>
-                <div className="h-auto max-h-[72px] flex justify-end gap-10px items-center gap-[16px] text-[#f8fafc]">
+                <div className="flex justify-start items-center">
+                  <Input placeholder="Search..." prefix={<SearchOutlined />} />
+                </div>
+                <div className={`h-auto max-h-[72px] flex justify-end gap-10px items-center gap-[16px] text-[${Colors.TEXT_COLOR}]`}>
                     <Dropdown menu={{items, onClick}}>
-                        <a className="hover:text-[#f8fafc]" onClick={(e)=>e.preventDefault()}>
+                        <a className={'hover:text-[#f8fafc]'} onClick={(e)=>e.preventDefault()}>
                           <Space>
                             {item.label}
                             <DownOutlined />
