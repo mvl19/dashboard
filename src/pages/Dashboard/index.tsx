d8ed34646ff125e59e74355a52ee142cd275ddd5
Dennis Destian <dominico.dennis@live.com>
2023-11-25 21:46:55 +0700

    feat: added dummy flex cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
-import { Row, Col } from 'antd'
+import { Flex } from 'antd'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 export default function Dashboard() {
 
     return (
         <div className="flex justify-center">
             <div className="w-[100%] h-screen max-h-[100%] flex flex-col items-center bg-darkbg p-[24px]">
-                {/* <Row gutter={{ xs:8, sm: 16, md: 24, lg: 32 }}>
-                    <Col></Col>
-                </Row> */}
-                <div className="grid grid-cols-3 gap-4">
-                    
+                <div className="border border-darkgrey border-1 p-[10px] gap-2 w-[98%] min-w-[320px] flex flex-wrap flex-row sm:justify-center md:justify-between items-center">
+                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
+                        Card #1
+                    </div>
+                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
+                        Card #2
+                    </div>
+                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
+                        Card #3
+                    </div>
                 </div>
+                <Flex wrap="wrap" gap='small' justify="space-between" className="border border-darkgrey border-1 p-[10px] w-[98%] min-w-[320px]">
+                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
+                        Card #1
+                    </div>
+                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
+                        Card #2
+                    </div>
+                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
+                        Card #3
+                    </div>
+                </Flex>
             </div>
         </div>
     )
