256ec0bf97306350980bd130293449a670d71451
Dennis Destian <dominico.dennis@live.com>
2023-11-25 12:19:05 +0700

    feat: added tailwind color theme

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { Row, Col } from 'antd'
 
 export default function Dashboard() {
 
     return (
         <div className="flex justify-center">
-            <div className="w-[100%] h-screen max-h-[100%] flex flex-col items-center bg-white p-[24px]">
-                <div className="text-black">
-                    This is Dashboard Page
+            <div className="w-[100%] h-screen max-h-[100%] flex flex-col items-center bg-darkbg p-[24px]">
+                {/* <Row gutter={{ xs:8, sm: 16, md: 24, lg: 32 }}>
+                    <Col></Col>
+                </Row> */}
+                <div className="grid grid-cols-3 gap-4">
+                    
                 </div>
             </div>
         </div>
