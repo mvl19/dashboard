611f919527646e5feba354d604dcf1485f04368b
mvl19 <56024956+mvl19@users.noreply.github.com>
2023-11-29 08:31:30 +0700

    feat: add text annotation to chart

--- src/components/Pie.tsx
+++ src/components/Pie.tsx
@@ -3,7 +3,8 @@ import { scaleOrdinal } from "@visx/scale"
 import { GradientPurpleTeal } from "@visx/gradient"
 import { Group } from "@visx/group"
 import Pie from '@visx/shape/lib/shapes/Pie'
-import { useTooltip } from "@visx/tooltip"
+import { Text } from "@visx/text"
+import { useState } from "react"
 
 interface Browsers {
     date: string;
@@ -43,6 +44,8 @@ interface PieProps {
     margin?: typeof defaultMargin,
     animate?: boolean,
     fontSize?: number,
+    fill?:string,
+    annotationFontSize:number,
 }
 
 export default function PieChart({
@@ -50,6 +53,8 @@ export default function PieChart({
     height,
     margin=defaultMargin,
     fontSize=14,
+    fill='#ffffff',
+    annotationFontSize=14
 }: PieProps) {
     const accessors = {
         usage: (d: BrowserUsage) => d.usage,
@@ -67,7 +72,7 @@ export default function PieChart({
         ]
     })
     if (width < 10) return null
-    const { tooltipData, tooltipLeft, tooltipTop, tooltipOpen, showTooltip, hideTooltip } = useTooltip()
+    const [active, setActive] = useState(null)
     const innerWidth = width - margin.left - margin.right
     const innerHeight = height - margin.top - margin.bottom
     const radius = Math.min(innerWidth, innerHeight) / 2
@@ -87,7 +92,6 @@ export default function PieChart({
               innerRadius={radius - donutThickness}
               cornerRadius={3}
               padAngle={0.005}
-              onMouseEnter={data => console.log(data)}
               >
                 {(pie)=>{
                     return pie.arcs.map((arc, index) => {
@@ -99,19 +103,15 @@ export default function PieChart({
                         return (
                             <g key={`arc-${label}-${index}`}>
                                 <path d={arcPath} fill={arcFill}
-                                onMouseEnter={()=>showTooltip({
-                                    tooltipData: arc.data,
-                                    tooltipLeft: centroidX,
-                                    tooltipTop: centroidY,
-                                })}
-                                onMouseLeave={hideTooltip}
+                                onMouseEnter={() => setActive(arc.data)}
+                                onMouseLeave={() => setActive(null)}
                                 />
                                 {hasSpaceForLabel && (
                                     <text
                                     x={centroidX}
                                     y={centroidY}
                                     dy=".33em"
-                                    fill="#ffffff"
+                                    fill={fill}
                                     fontSize={fontSize}
                                     textAnchor="middle"
                                     pointerEvents={"none"}>
@@ -123,22 +123,18 @@ export default function PieChart({
                     })
                 }}
             </Pie>
+            {active ? (
+                <>
+                <Text textAnchor="middle" fill="#ffffff" fontSize={annotationFontSize} dy={-10}>
+                    {active.label + " " + active.usage + "%"}
+                </Text>
+                </>
+            ): (
+                <>
+                </>
+            )}
             </Group>
         </svg>
-        {tooltipOpen && (
-            <div
-            className="tooltip"
-            style={{
-              position: "absolute",
-              top: (height) ,
-              left: tooltipLeft + width / 2,
-              pointerEvents: "none",
-              background: "rgba(0,0,0,0.3)"
-            }}
-          >
-            {JSON.stringify(tooltipData, undefined, "\n")}
-          </div>
-        )}
         </div>
     )
 }
