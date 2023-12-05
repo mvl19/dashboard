fb75500acf6c1e71ef9c0d9e4a9888418afe11c6
mvl19 <56024956+mvl19@users.noreply.github.com>
2023-12-05 07:55:52 +0700

    feat: horizontal bar chart

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import LineChart from "../../components/Line"
 import PieChart from "../../components/Pie"
-import { salaryData, spendingData } from "../../data"
+import HorizontalBar from "../../components/Bar"
+import { bar, salaryData, spendingData } from "../../data"
 import { ParentSize } from "@visx/responsive"
 import { letterFrequency } from "../../data"
 
@@ -9,7 +10,9 @@ export default function Dashboard() {
     return (
         <div className="flex justify-center">
             <div className="w-[100%] h-screen max-h-[100%] flex flex-col items-center bg-white p-[24px]">
-                <LineChart data={[spendingData, salaryData]}/>
+                <ParentSize>
+                    {({ width, height }) =><HorizontalBar width={width} height={height} />}
+                </ParentSize>
                 <ParentSize>
                     {({ width, height }) =><PieChart width={width} height={height} dataset={letterFrequency} accessor="frequency" labels="letter" />}
                 </ParentSize>
