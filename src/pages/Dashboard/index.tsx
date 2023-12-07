34024754056b763581309d4ff054d1275a16d5fa
Dennis Destian <dominico.dennis@live.com>
2023-12-07 08:55:33 +0700

    fix: removed placeholder flex & border

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -10,11 +10,11 @@ const firstRow = [
     },
     {
         id: '2',
-        name: 'Bar Chart'
+        name: 'Score Card'
     },
     {
         id: '3',
-        name: 'Pie Chart'
+        name: 'Score Card'
     }
 ]
 
@@ -44,21 +44,10 @@ export default function Dashboard() {
     return (
         <div className="flex justify-center">
             <div className="w-[100%] h-screen max-h-[100%] flex flex-col items-center bg-darkbg p-[24px]">
-                <div className="border border-darkgrey border-1 p-[10px] gap-2 w-[98%] min-w-[320px] flex flex-wrap flex-row sm:justify-center md:justify-between items-center">
-                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
-                        Card #1
-                    </div>
-                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
-                        Card #2
-                    </div>
-                    <div className="bg-darkitem h-[90px] lg:basis-[32%] md:basis-auto min-w-[300px] flex justify-center items-center">
-                        Card #3
-                    </div>
-                </div>
                 <DragDropContext onDragEnd={handleDragDrop}>
                     <Droppable droppableId='rowOne' type='group' direction="horizontal">
                         {(provided: any) => (
-                            <Flex wrap="wrap" gap='small' justify="space-between" className="border border-darkgrey border-1 p-[10px] w-[98%] min-w-[320px]" {...provided.droppableProps} ref={provided.innerRef}>            
+                            <Flex wrap="wrap" gap='small' justify="space-between" className="p-[10px] w-[98%] min-w-[320px]" {...provided.droppableProps} ref={provided.innerRef}>            
                                 {
                                     rowOneItems.map((item, idx) => (
                                         <Draggable draggableId={item.id} key={item.id} index={idx}>
